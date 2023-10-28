952ac12938fe759a755550322fc31a77f69ba5c8
Francori8 <francoorizonte@gmail.com>
2023-10-28 11:49:07 -0300

    Se agrego un manejo de error minimo del fetch

--- src/script.js
+++ src/script.js
@@ -1,8 +1,84 @@
+const data = {
+  "results": [
+    {
+      "gender": "female",
+      "name": {
+        "title": "Miss",
+        "first": "Jennie",
+        "last": "Nichols"
+      },
+      "location": {
+        "street": {
+          "number": 8929,
+          "name": "Valwood Pkwy",
+        },
+        "city": "Billings",
+        "state": "Michigan",
+        "country": "United States",
+        "postcode": "63104",
+        "coordinates": {
+          "latitude": "-69.8246",
+          "longitude": "134.8719"
+        },
+        "timezone": {
+          "offset": "+9:30",
+          "description": "Adelaide, Darwin"
+        }
+      },
+      "email": "jennie.nichols@example.com",
+      "login": {
+        "uuid": "7a0eed16-9430-4d68-901f-c0d4c1c3bf00",
+        "username": "yellowpeacock117",
+        "password": "addison",
+        "salt": "sld1yGtd",
+        "md5": "ab54ac4c0be9480ae8fa5e9e2a5196a3",
+        "sha1": "edcf2ce613cbdea349133c52dc2f3b83168dc51b",
+        "sha256": "48df5229235ada28389b91e60a935e4f9b73eb4bdb855ef9258a1751f10bdc5d"
+      },
+      "dob": {
+        "date": "1992-03-08T15:13:16.688Z",
+        "age": 30
+      },
+      "registered": {
+        "date": "2007-07-09T05:51:59.390Z",
+        "age": 14
+      },
+      "phone": "(272) 790-0888",
+      "cell": "(489) 330-2385",
+      "id": {
+        "name": "SSN",
+        "value": "405-88-3636"
+      },
+      "picture": {
+        "large": "https://randomuser.me/api/portraits/men/75.jpg",
+        "medium": "https://randomuser.me/api/portraits/med/men/75.jpg",
+        "thumbnail": "https://randomuser.me/api/portraits/thumb/men/75.jpg"
+      },
+      "nat": "US"
+    }
+  ],
+  "info": {
+    "seed": "56d27f4a53bd5441",
+    "results": 1,
+    "page": 1,
+    "version": "1.4"
+  }
+}
+
+
 
 function extraerDatosWeb(){
-  fetch("https://randomuser.me/api/")
-  .then((response) => response.json())
-  .then((data) => app(data))
+  
+    fetch("https://randomuser.me/api/")
+    .then((response) => response.json())
+    .then((data) => app(data))
+    .catch((error) => {
+       
+        app(data)
+      }
+    )
+    
+    
 }
 
 
@@ -17,73 +93,7 @@ function $$(arg){
     return document.querySelectorAll(arg)
 }
 
- const data = {
-    "results": [
-      {
-        "gender": "female",
-        "name": {
-          "title": "Miss",
-          "first": "Jennie",
-          "last": "Nichols"
-        },
-        "location": {
-          "street": {
-            "number": 8929,
-            "name": "Valwood Pkwy",
-          },
-          "city": "Billings",
-          "state": "Michigan",
-          "country": "United States",
-          "postcode": "63104",
-          "coordinates": {
-            "latitude": "-69.8246",
-            "longitude": "134.8719"
-          },
-          "timezone": {
-            "offset": "+9:30",
-            "description": "Adelaide, Darwin"
-          }
-        },
-        "email": "jennie.nichols@example.com",
-        "login": {
-          "uuid": "7a0eed16-9430-4d68-901f-c0d4c1c3bf00",
-          "username": "yellowpeacock117",
-          "password": "addison",
-          "salt": "sld1yGtd",
-          "md5": "ab54ac4c0be9480ae8fa5e9e2a5196a3",
-          "sha1": "edcf2ce613cbdea349133c52dc2f3b83168dc51b",
-          "sha256": "48df5229235ada28389b91e60a935e4f9b73eb4bdb855ef9258a1751f10bdc5d"
-        },
-        "dob": {
-          "date": "1992-03-08T15:13:16.688Z",
-          "age": 30
-        },
-        "registered": {
-          "date": "2007-07-09T05:51:59.390Z",
-          "age": 14
-        },
-        "phone": "(272) 790-0888",
-        "cell": "(489) 330-2385",
-        "id": {
-          "name": "SSN",
-          "value": "405-88-3636"
-        },
-        "picture": {
-          "large": "https://randomuser.me/api/portraits/men/75.jpg",
-          "medium": "https://randomuser.me/api/portraits/med/men/75.jpg",
-          "thumbnail": "https://randomuser.me/api/portraits/thumb/men/75.jpg"
-        },
-        "nat": "US"
-      }
-    ],
-    "info": {
-      "seed": "56d27f4a53bd5441",
-      "results": 1,
-      "page": 1,
-      "version": "1.4"
-    }
-  }
-
+ 
 
 function visualizacionDeDatosEscrito(data){
   const cambioData = {
@@ -102,8 +112,6 @@ function visualizacionDeDatosEscrito(data){
 
 
 function visualizacionDeDatosGrafica(e){
-
-
   $$(".itemDato").forEach((elemento) => {
     elemento.classList.remove("active")
   })
@@ -127,6 +135,7 @@ function incorporarDatosCV(data){
 })}
 
 function app(data){
+    
     const infoData = data.info || undefined
     if(infoData){
         $("#version").innerHTML = ` v ~ ${infoData.version}`
